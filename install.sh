4a9f0df0e9b9f3fb71b8ceef7977f9007c952e20
hyeonsu.Im <yis1308@gmail.com>
2022-10-13 00:24:33 +0900

    clean code and env settings.

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-sudo apt-get install python3
-sudo apt-get install python3-pip
+#!/bin/bin/env bash
+if [ "$(uname)" == "Darwin" ] 
+then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+    brew install python3
+    brew install python3-pip
+else
+    sudo apt-get install python3
+    sudo apt-get install python3-pip
+fi
 
 pip3 install -r requiremenets.txt
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from flask import Flask, abort, render_template, request
-import datetime
-import holidays
+"""Web server main"""
+from datetime import datetime
+from flask import Flask, render_template
+from holidays import KR
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def show_main_view():
-    date = datetime.datetime.now()
+    """ rendering main page"""
+    date = datetime.now()
     holidays = get_holiday_lists(date.year)
     #print(holidays)
     return render_template('html/main.html', data=holidays)
 
 def get_holiday_lists(year):
-    return list(holidays.KR(years=year).keys())
+    """ Get holidays"""
+    return list(KR(years=year).keys())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=20000)
