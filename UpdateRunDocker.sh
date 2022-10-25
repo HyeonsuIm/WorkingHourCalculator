cc6f98b20f162662692687b76462bd9a73db025d
hyeonsu.Im <yis1308@gmail.com>
2022-10-26 00:22:28 +0900

    move files and add nginx server

--- UpdateRunDocker.sh
+++ UpdateRunDocker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-NODE_NAME="working-calendar-server"
-docker stop $NODE_NAME
-docker rm $NODE_NAME
+#NODE_NAME="working-calendar-server"
+#docker stop $NODE_NAME
+#docker rm $NODE_NAME
+#
+#git pull
+#docker build -t $NODE_NAME ./appserver
+#docker run --name $NODE_NAME -p 20000:20000 $NODE_NAME &
 
 git pull
-docker build -t $NODE_NAME .
-docker run --name $NODE_NAME -p 20000:20000 $NODE_NAME &
+docker-compose up -d --build
