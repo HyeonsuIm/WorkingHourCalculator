3528d71d641adc8cb20546551a22e98635a5c6db
hyeonsu.Im <yis1308@gmail.com>
2022-12-03 11:55:54 +0900

    Update function add function for updating previous work hour for each days

--- appserver/static/Calendar/CalendarViewHandler.js
+++ appserver/static/Calendar/CalendarViewHandler.js
@@ -1,6 +1,20 @@
 function displayModal(element) {
     var keyVal = element.getAttribute('data-id');
+
+    year_month_day = keyVal.split('-')
+    let workingHours = GetWorkingHour(year_month_day[0], year_month_day[1])
     $(".modal-body #keyVal").val(keyVal)
+
+    let working_hour_element = $("#work_hour_day")
+    day = Number(year_month_day[2])
+    if(workingHours[day]){
+        str = String(Math.floor(workingHours[day] / 60)) + ":" + String(workingHours[day] % 60).padStart(2,'0')
+        working_hour_element.val(str)
+    }
+    else{
+        working_hour_element.val("")
+        working_hour_element.attr('placeholder', '10:00')
+    }
     $('#day_modal').modal('show')
 }
 
@@ -38,8 +52,20 @@ function UpdateAllViews() {
 }
 
 function UpdateDayInformationFromPopup() {
-    let element = document.getElementById("keyVal")
-    keyVal = element.value
+    let keyVal = document.getElementById("keyVal").value
+    let working_hour = document.getElementById('work_hour_day').value
+    if(working_hour != '')
+    {
+        let times = working_hour.split(':')
+        let minute = 0;
+        if( times.length == 2 )
+        {
+            minute = times[0]*60 + times[1]*1
+        }
+        year_month_day = keyVal.split('-')
+        updateWorkingHour(Number(year_month_day[0]), Number(year_month_day[1]), Number(year_month_day[2]), minute)
+        UpdateAllViews()
+    }
 
     if (document.getElementById("full_day").checked) {
         UpdateDayInfo(keyVal, 'full_day')
