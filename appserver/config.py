8d7104be3c54b66cef96b7a4f9b0c8ec4f281a71
hyeonsu.Im <yis1308@gmail.com>
2022-12-04 00:59:54 +0900

    Fix docker server problems

--- appserver/config.py
+++ appserver/config.py
@@ -4,7 +4,9 @@ db = {
     # 사용자 비밀번호
     'password': '1',
     # 접속할 데이터베이스의 주소 (같은 컴퓨터에 있는 데이터베이스에 접속하기 때문에 localhost)
-    'host': 'dbserver',
+    'host': '127.0.0.1',
+    # comments for dev task on the mac
+    #'host': 'dbserver',
     # 관계형 데이터베이스는 주로 3306 포트를 통해 연결됨
     'port': 3306,
     # 실제 사용할 데이터베이스 이름
